51e3fc0ea6f742b4ef4e38f81d70f7e89a65a5d3
Samuel Teich <steich@gmail.com>
2017-11-15 12:26:29 -0600

    Don't think the controller layers's weights are training

--- MANN_LSTM.py
+++ MANN_LSTM.py
@@ -98,7 +98,7 @@ class MANN_LSTMCell(Layer):
         self.write_gate_initializer = write_gate_initializer
         self.write_gate_regularizer = write_gate_regularizer
         self.write_gate_constraint = write_gate_constraint
-        self.state_size = tuple([None for i in range(6)]) + self.Controller.cell.state_size
+        self.state_size = tuple([None for i in range(5)]) + self.Controller.cell.state_size
                 
     def build(self, input_shape):
                         
@@ -145,11 +145,10 @@ class MANN_LSTMCell(Layer):
         template = K.tile(template, [1, self.Controller.units]) #(samples, units)
 
         r_tm1 = K.zeros_like(template)
-        m_tm1 = K.ones((self.memory_size, self.Controller.units), dtype=tf.float32)
+        m_tm1 = K.ones((self.memory_size, self.Controller.units), dtype=tf.float32) * 1e-6
         c_wu_tm1 = K.dot(K.zeros((self.memory_size, 1)), z_temp)
         c_wlu_tm1 = K.dot(K.zeros((self.memory_size, 1)), z_temp)
         c_wr_tm1 = K.dot(K.zeros((self.memory_size, 1)), z_temp)
-        c_ww_tm1 = K.dot(K.zeros((self.memory_size, 1)), z_temp)
 
         c_initial_states = self.Controller.get_initial_state(K.concatenate([inputs, K.expand_dims(r_tm1, axis=1)]))
 
@@ -157,11 +156,10 @@ class MANN_LSTMCell(Layer):
                             m_tm1.shape,
                             c_wu_tm1.shape,
                             c_wlu_tm1.shape,
-                            c_wr_tm1.shape,
-                            c_ww_tm1.shape] + \
+                            c_wr_tm1.shape] + \
                             [state.shape for state in c_initial_states]
 
-        return [r_tm1, m_tm1, c_wu_tm1, c_wlu_tm1, c_wr_tm1, c_ww_tm1] + \
+        return [r_tm1, m_tm1, c_wu_tm1, c_wlu_tm1, c_wr_tm1] + \
                 [state for state in c_initial_states]
             
     def call(self, inputs, states, training=None):
@@ -171,15 +169,10 @@ class MANN_LSTMCell(Layer):
         c_wu_tm1 = states[2]
         c_wlu_tm1 = states[3]
         c_wr_tm1 = states[4]
-        c_ww_tm1 = states[5]
 
-        controller_states = states[6:]
+        controller_states = states[5:]
         controller_inputs = K.concatenate([inputs, r_tm1])
         key_list, controller_states = self.Controller.cell.call(controller_inputs, controller_states, training=training)
-
-        #we want (keys, batches) so we can figure out read weights 
-        #for each sample in the batch
-        key_list = K.transpose(key_list) #(units, batch_size)
 
         #the write weight is only dependent on last cycles states
         c_ww = K.sigmoid(self.write_gate) * c_wr_tm1 + \
@@ -187,30 +180,34 @@ class MANN_LSTMCell(Layer):
 
         #here we find the cosine similarity between keys and memory rows
         #for each batch's key, and then softmax that to find the read weight
-        n_key = K.l2_normalize(key_list, 0) 
-        n_memory = K.l2_normalize(m_tm1, 1)
-        mem_cos_similarity = K.dot(n_memory, n_key)
-        c_wr = K.softmax(mem_cos_similarity)
+        n_key = K.l2_normalize(key_list, 1) #(batch, units), normed rows
+        n_memory = K.l2_normalize(m_tm1, 1) #(memory, units), normed rows
+        mem_cos_similarity = K.dot(n_key, K.transpose(n_memory)) #(batch, memory)
+        c_wr = K.softmax(K.transpose(mem_cos_similarity)) #(memory, batch)
 
         #the read value for each sample is the sum of the weight adjusted memory rows
-        read = K.dot(tf.transpose(m_tm1), c_wr) #(units, memory) x (memory, samples)
-        read = K.transpose(read) #(samples, units)
+        read = K.dot(K.transpose(c_wr), m_tm1) #(batch, memory) x (memory, units) = (batch, units)
 
         #figure out how much each row in memory got used for each sample in batch
-        c_wu = self.usage_decay * c_wu_tm1 + c_wr + c_ww
+        c_wu = self.usage_decay * c_wu_tm1 + c_wr + c_ww #(memory, batch)
 
+        #n is one atm, need to implement a variable number of read heads
         #grab the smallest usage value for each sample in the batch, and the index of that row
-        v, i = tf.nn.top_k(tf.transpose(c_wu), self.memory_size) #v, i are (batch_size, memory)
+        v, i = tf.nn.top_k(K.transpose(c_wu), self.memory_size) #v, i are (batch_size, memory)
         nth_smallest = K.reshape(K.transpose(v)[-1,:], [-1]) #(batch_size)
+        i_nth_smallest = K.argmin(nth_smallest) #index of minimum value in nth_smallest array
         nth_smallest = tf.expand_dims(nth_smallest, axis = 1) #(batch_size, 1)
         nth_smallest = tf.tile(nth_smallest, [1, self.memory_size]) #(batch_size, self.memory)
         nth_smallest_i = K.reshape(K.transpose(i)[-1:], [-1]) #(batch_size)
-        lt = K.less_equal(c_wu, K.transpose(nth_smallest))
+        i = nth_smallest_i[i_nth_smallest] #index of min nth_smallest in c_wu
+        nth_smallest_i = K.ones_like(nth_smallest_i) * i #(batch_size) of replicated i
+        lt = K.less_equal(c_wu, K.transpose(nth_smallest)) #(memory_size, batch_size)
         c_wlu = K.cast(lt, tf.float32)
         #zero out each sample's least used row in the batch
-        zeroing_vector = tf.one_hot(nth_smallest_i, self.memory_size, on_value = 0., off_value = 1., axis = 0) #(memory, batch)
-        ones_vector = K.ones_like(tf.matmul(tf.transpose(zeroing_vector), tf.ones((self.memory_size, self.Controller.units)))) 
-        memory = K.dot(zeroing_vector, ones_vector) * m_tm1
-        memory = K.dot(c_ww, tf.transpose(key_list)) + memory
+        zeroing_matrix = tf.one_hot(nth_smallest_i, self.memory_size, on_value = 0., off_value = 1., axis = 0) #(memory, batch)
+        #zeroing_matrix should have a zero row
+        ones_matrix = K.ones_like(tf.matmul(tf.transpose(zeroing_matrix), tf.ones((self.memory_size, self.Controller.units)))) 
+        memory = K.dot(zeroing_matrix, ones_matrix) * m_tm1
+        memory = K.dot(c_ww, key_list) + memory
  
-        return read, [read, memory, c_wu, c_wlu, c_wr, c_ww] + controller_states
+        return read, [read, memory, c_wu, c_wlu, c_wr] + controller_states
